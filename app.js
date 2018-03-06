e48d3c93daf842918d1e13fbbddd6ca21d7d4065
GSSchneider <35821280+GSSchneider@users.noreply.github.com>
2018-03-06 12:20:03 -0500

    Get req path and method, etc., to log to console for every req instance

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const chalk = require('chalk');
+const log = console.log;
 const app = express(); // creates an instance of an express application
 
-app.listen(3000, () => {
-  console.log('server listening');
+app.use('/', (req, res, next) => {
+  log(chalk.magenta(req.method + ' ' + req.path)); //+ chalk.cyan(`/ ${req.params.something}`));
+  next();
 });
 
 app.get('/', (req, res) => {
   res.send('Sup.');
 });
 
+app.get('/news', (req, res) => {
+  res.send('No news to be found here.');
+});
+
+app.post('/post', function (req, res) {
+  res.send('POST request to homepage');
+});
+
+app.listen(3000, () => {
+  console.log('server listening');
+});
