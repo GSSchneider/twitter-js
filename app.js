b5a92cd1f9b0f43ccf2cbad1d4387460678a7550
Lara Pollack <larpollack@gmail.com>
2018-03-06 12:56:19 -0500

    nunjucks nunchucks!

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const chalk = require('chalk');
+const nunjucks = require('nunjucks');
 const log = console.log;
 const app = express(); // creates an instance of an express application
+
+const things = [{item: 'Ladder'}, {item: 'Mirror'}, {item: 'Cat'}];
+
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates; not caching
+
+
 
 app.use('/', (req, res, next) => {
   log(chalk.magenta(req.method + ' ' + req.path)); //+ chalk.cyan(`/ ${req.params.something}`));
@@ -9,7 +19,7 @@ app.use('/', (req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Sup.');
+  res.render( 'index', {title: 'Danger Zone', things: things} );
 });
 
 app.get('/news', (req, res) => {
